8b9b6f61c54b95bbaf414f85badb7cd02677a3eb
gchen <genhan.chen@azgs.az.gov>
2012-05-16 17:28:28 -0700

    Delete 'toGeographicCoords' function since the layers in GeoServer have been projected into 'ESPG:4326'

--- js/azgs-leaflet/GeoJSON.WFS.js
+++ js/azgs-leaflet/GeoJSON.WFS.js
@@ -3,7 +3,7 @@ L.GeoJSON.WFS = L.GeoJSON.extend({
 		options = options || {};
 		L.GeoJSON.prototype.initialize.call(this, null, options);
 		
-		var wfsVersion = options.wfsVersion || "1.1.0";
+		var wfsVersion = options.wfsVersion || "1.0.0";
 		this.getFeatureUrl = serviceUrl + "?request=GetFeature&outputformat=json&version=" + wfsVersion + "&typeName=" + featureType;
 		if (options.filter && options.filter instanceof DateFilter) { this.getFeatureUrl += "&CQL_FILTER=" + options.filter.cql; }
 		
@@ -55,9 +55,11 @@ L.GeoJSON.WFS = L.GeoJSON.extend({
 			if (options.hoverObj || options.hoverFld) {
 				e.layer.on("mouseover", function(evt) {
 					hoverContent = options.hoverObj ? options.hoverObj.generateContent(e) : e.properties[options.hoverFld] || "Invalid field name" ;
-					if(evt.target.hasOwnProperty("_latlng")){
+					
+					///
+					if(evt.target.hasOwnProperty("_latlng")){ ///For point
 						hoverPoint = e.layer._map.latLngToContainerPoint(evt.target._latlng);
-					}else if(evt.hasOwnProperty("latlng")){
+					}else if(evt.hasOwnProperty("latlng")){ ///For line
 						hoverPoint = e.layer._map.latLngToContainerPoint(evt.latlng);
 					}
 					
@@ -97,60 +99,13 @@ L.GeoJSON.WFS = L.GeoJSON.extend({
 			success: function(response) {
 				if (response.type && response.type == "FeatureCollection") {
 					that.jsonData = response;
-					
-					var inputCrs = that.jsonData.crs.type + ":" + that.jsonData.crs.properties.code;
-					if(inputCrs == "EPSG:4326"){
-						that.options.inputCrs = inputCrs;
-					}					
-					that.toGeographicCoords(that.options.inputCrs || "EPSG:900913");
+	
 					callback();
 				}				
 			},
 			dataType: "json"
 		});
-	},
-	
-	toGeographicCoords: function(inputCrs) {
-		function projectPoint(coordinates /* [x,y] */, inputCrs) {
-			var source = new Proj4js.Proj(inputCrs || "EPSG:900913"),
-				dest = new Proj4js.Proj("EPSG:4326");
-			
-			///Identify which coordinate is for latitude or longitude
-			if(Math.abs(coordinates[0]) > 90){
-				var x = coordinates[0], 
-				y = coordinates[1];				
-			}else{
-				var x = coordinates[1], 
-				y = coordinates[0];				
-			}
-
-			p = new Proj4js.Point(x,y);
-			Proj4js.transform(source, dest, p);
-			return [p.x, p.y];
-		}
-		
-		features = this.jsonData.features || [];
-		for (var f = 0; f < features.length; f++) {
-			switch (features[f].geometry.type) {
-				case "Point":
-					projectedCoords = projectPoint(features[f].geometry.coordinates, inputCrs);
-					features[f].geometry.coordinates = projectedCoords;
-					break;
-				case "MultiPoint":
-					for (var p = 0; p < features[f].geometry.coordinates.length; p ++) {
-						projectedCoords = projectPoint(features[f].geometry.coordinates[p], inputCrs);
-						features[f].geometry.coordinates[p] = projectedCoords;
-					}
-					break;
-				case "MultiLineString":
-					for (var c = 0; c < features[f].geometry.coordinates.length; c ++) {
-						for (var cc = 0; cc < features[f].geometry.coordinates[c].length; cc ++) {
-							var pCoords = features[f].geometry.coordinates[c][cc];
-							projectedCoords = projectPoint(pCoords, inputCrs);
-							features[f].geometry.coordinates[c][cc] = projectedCoords;
-						}
-					}
-			}
-		}
 	}
+	
+
 });
